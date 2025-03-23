d0dac7582a428c8a90fd0bca774bc92de63c7bad
Bemental77 <bementcw@gmail.com>
2025-03-23 12:36:57 -0500

    pathingFoxedUp

--- mkScoreboard/src/index.tsx
+++ mkScoreboard/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './App.css';
+import App from '../../mkScoreboard/src/pp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
