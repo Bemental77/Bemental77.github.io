bc70d6d723481476955956a601d60ab17aedba90
Bemental77 <bementcw@gmail.com>
2026-04-11 18:26:10 -0500

    romlist

--- gba/gbaWasm/dist/romlist.js
+++ gba/gbaWasm/dist/romlist.js
@@ -3,5 +3,6 @@ var ROMLIST = [
   {url:"gba/gbaWasm/roms/Sim City 2000 (U).gba",title:"Sim City 2000"},
   {url:"gba/gbaWasm/roms/Sonic Advance 3 (U).gba",title:"Sonic Advance 3"},
   {url:"gba/gbaWasm/roms/Super Bust-A-Move (U).gba",title:"Super Bust-A-Move"},
-  {url:"gba/gbaWasm/roms/Kirby - Nightmare in Dream Land (U).gba",title:"Kirby Nightmare in Dreamland"}
+  {url:"gba/gbaWasm/roms/Kirby - Nightmare in Dream Land (U).gba",title:"Kirby Nightmare in Dreamland"},
+  {url:"gba/gbaWasm/roms/Monster Rancher Advance 2 (U).gba",title:"Monster Rancher Advance 2"}
 ];
