c49d1b64fe69579dfaba511c4bf3589682d1fc34
Bemental77 <bementcw@gmail.com>
2026-04-11 11:28:53 -0500

    controller tests

--- gba/gbaWasm/dist/input_controller.js
+++ gba/gbaWasm/dist/input_controller.js
@@ -55,18 +55,94 @@ class InputController {
             Mapping_Action_L: 'q',
             Mapping_Action_R: 'e',
             Mapping_Menu: '`',
+
             Joy_Mapping_Left: 14,
             Joy_Mapping_Right: 15,
             Joy_Mapping_Down: 13,
             Joy_Mapping_Up: 12,
+
             Joy_Mapping_Action_A: 0,
             Joy_Mapping_Action_B: 1,
             Joy_Mapping_Action_Start: 9,
             Joy_Mapping_Action_Select: 8,
-            Joy_Mapping_Action_L: 4,
-            Joy_Mapping_Action_R: 5,
-            Joy_Mapping_Menu: 11,
-        };
+
+            Joy_Mapping_Action_L: 6,
+            Joy_Mapping_Action_R: 7,
+
+            Joy_Mapping_Menu: 16
+        }
+    }
+
+    processGamepad() {
+        try {
+            const gamepads = navigator.getGamepads ? navigator.getGamepads() : []
+            if (!gamepads) return
+
+            let gp = null
+            for (let i = 0; i < gamepads.length; i++) {
+                if (gamepads[i]) {
+                    gp = gamepads[i]
+                    break
+                }
+            }
+
+            if (!gp) return
+
+            const btn = n => gp.buttons[n] && gp.buttons[n].pressed
+
+            const set = (flag, joyBtn, keyMapping) => {
+                if (btn(joyBtn)) {
+                    if (!this[flag]) this.sendKeyDownEvent(this.KeyMappings[keyMapping])
+                } else {
+                    if (this[flag]) this.sendKeyUpEvent(this.KeyMappings[keyMapping])
+                }
+            }
+
+            set('Key_Action_A', this.KeyMappings.Joy_Mapping_Action_A, 'Mapping_Action_A')
+            set('Key_Action_B', this.KeyMappings.Joy_Mapping_Action_B, 'Mapping_Action_B')
+            set('Key_Action_Start', this.KeyMappings.Joy_Mapping_Action_Start, 'Mapping_Action_Start')
+            set('Key_Action_Select', this.KeyMappings.Joy_Mapping_Action_Select, 'Mapping_Action_Select')
+            set('Key_Action_L', this.KeyMappings.Joy_Mapping_Action_L, 'Mapping_Action_L')
+            set('Key_Action_R', this.KeyMappings.Joy_Mapping_Action_R, 'Mapping_Action_R')
+
+            let up = btn(this.KeyMappings.Joy_Mapping_Up)
+            let down = btn(this.KeyMappings.Joy_Mapping_Down)
+            let left = btn(this.KeyMappings.Joy_Mapping_Left)
+            let right = btn(this.KeyMappings.Joy_Mapping_Right)
+
+            const ax0 = gp.axes[0] || 0
+            const ax1 = gp.axes[1] || 0
+
+            if (ax0 < -0.4) left = true
+            if (ax0 > 0.4) right = true
+            if (ax1 < -0.4) up = true
+            if (ax1 > 0.4) down = true
+
+            if (up) {
+                if (!this.Key_Up) this.sendKeyDownEvent(this.KeyMappings.Mapping_Up)
+            } else {
+                if (this.Key_Up) this.sendKeyUpEvent(this.KeyMappings.Mapping_Up)
+            }
+
+            if (down) {
+                if (!this.Key_Down) this.sendKeyDownEvent(this.KeyMappings.Mapping_Down)
+            } else {
+                if (this.Key_Down) this.sendKeyUpEvent(this.KeyMappings.Mapping_Down)
+            }
+
+            if (left) {
+                if (!this.Key_Left) this.sendKeyDownEvent(this.KeyMappings.Mapping_Left)
+            } else {
+                if (this.Key_Left) this.sendKeyUpEvent(this.KeyMappings.Mapping_Left)
+            }
+
+            if (right) {
+                if (!this.Key_Right) this.sendKeyDownEvent(this.KeyMappings.Mapping_Right)
+            } else {
+                if (this.Key_Right) this.sendKeyUpEvent(this.KeyMappings.Mapping_Right)
+            }
+
+        } catch (e) { }
     }
 
     setupMobileControls(touch_element_id) {
@@ -102,12 +178,12 @@ class InputController {
                 document.getElementById(id).addEventListener('touchmove', e => e.preventDefault(), false);
             };
 
-            bind('mobileA',      this.mobilePressA,      this.mobileReleaseA);
-            bind('mobileB',      this.mobilePressB,      this.mobileReleaseB);
-            bind('mobileStart',  this.mobilePressStart,  this.mobileReleaseStart);
+            bind('mobileA', this.mobilePressA, this.mobileReleaseA);
+            bind('mobileB', this.mobilePressB, this.mobileReleaseB);
+            bind('mobileStart', this.mobilePressStart, this.mobileReleaseStart);
             bind('mobileSelect', this.mobilePressSelect, this.mobileReleaseSelect);
-            bind('mobileL',      this.mobilePressL,      this.mobileReleaseL);
-            bind('mobileR',      this.mobilePressR,      this.mobileReleaseR);
+            bind('mobileL', this.mobilePressL, this.mobileReleaseL);
+            bind('mobileR', this.mobilePressR, this.mobileReleaseR);
 
             document.getElementById('menuDiv').addEventListener('touchstart', this.menuTouch.bind(this), false);
         }
@@ -118,19 +194,19 @@ class InputController {
         $('#menuDiv').hide();
     }
 
-    mobilePressA(e)      { e.preventDefault(); this.Key_Action_A = true;      this.MobileA = true; }
-    mobilePressB(e)      { e.preventDefault(); this.Key_Action_B = true;      this.MobileB = true; }
-    mobilePressStart(e)  { e.preventDefault(); this.Key_Action_Start = true;  this.MobileStart = true; }
+    mobilePressA(e) { e.preventDefault(); this.Key_Action_A = true; this.MobileA = true; }
+    mobilePressB(e) { e.preventDefault(); this.Key_Action_B = true; this.MobileB = true; }
+    mobilePressStart(e) { e.preventDefault(); this.Key_Action_Start = true; this.MobileStart = true; }
     mobilePressSelect(e) { e.preventDefault(); this.Key_Action_Select = true; this.MobileSelect = true; }
-    mobilePressL(e)      { e.preventDefault(); this.Key_Action_L = true;      this.MobileL = true; }
-    mobilePressR(e)      { e.preventDefault(); this.Key_Action_R = true;      this.MobileR = true; }
+    mobilePressL(e) { e.preventDefault(); this.Key_Action_L = true; this.MobileL = true; }
+    mobilePressR(e) { e.preventDefault(); this.Key_Action_R = true; this.MobileR = true; }
 
-    mobileReleaseA(e)      { e.preventDefault(); this.Key_Action_A = false;      this.MobileA = false; }
-    mobileReleaseB(e)      { e.preventDefault(); this.Key_Action_B = false;      this.MobileB = false; }
-    mobileReleaseStart(e)  { e.preventDefault(); this.Key_Action_Start = false;  this.MobileStart = false; }
+    mobileReleaseA(e) { e.preventDefault(); this.Key_Action_A = false; this.MobileA = false; }
+    mobileReleaseB(e) { e.preventDefault(); this.Key_Action_B = false; this.MobileB = false; }
+    mobileReleaseStart(e) { e.preventDefault(); this.Key_Action_Start = false; this.MobileStart = false; }
     mobileReleaseSelect(e) { e.preventDefault(); this.Key_Action_Select = false; this.MobileSelect = false; }
-    mobileReleaseL(e)      { e.preventDefault(); this.Key_Action_L = false;      this.MobileL = false; }
-    mobileReleaseR(e)      { e.preventDefault(); this.Key_Action_R = false;      this.MobileR = false; }
+    mobileReleaseL(e) { e.preventDefault(); this.Key_Action_L = false; this.MobileL = false; }
+    mobileReleaseR(e) { e.preventDefault(); this.Key_Action_R = false; this.MobileR = false; }
 
     setGamePadButtons() {
         this.gamepadButtons = [];
@@ -167,31 +243,31 @@ class InputController {
                     }
                 };
 
-                set('Key_Up',           this.KeyMappings.Joy_Mapping_Up,            'Mapping_Up');
-                set('Key_Down',         this.KeyMappings.Joy_Mapping_Down,          'Mapping_Down');
-                set('Key_Left',         this.KeyMappings.Joy_Mapping_Left,          'Mapping_Left');
-                set('Key_Right',        this.KeyMappings.Joy_Mapping_Right,         'Mapping_Right');
-                set('Key_Action_A',     this.KeyMappings.Joy_Mapping_Action_A,      'Mapping_Action_A');
-                set('Key_Action_B',     this.KeyMappings.Joy_Mapping_Action_B,      'Mapping_Action_B');
-                set('Key_Action_Start', this.KeyMappings.Joy_Mapping_Action_Start,  'Mapping_Action_Start');
-                set('Key_Action_Select',this.KeyMappings.Joy_Mapping_Action_Select, 'Mapping_Action_Select');
-                set('Key_Action_L',     this.KeyMappings.Joy_Mapping_Action_L,      'Mapping_Action_L');
-                set('Key_Action_R',     this.KeyMappings.Joy_Mapping_Action_R,      'Mapping_Action_R');
+                set('Key_Up', this.KeyMappings.Joy_Mapping_Up, 'Mapping_Up');
+                set('Key_Down', this.KeyMappings.Joy_Mapping_Down, 'Mapping_Down');
+                set('Key_Left', this.KeyMappings.Joy_Mapping_Left, 'Mapping_Left');
+                set('Key_Right', this.KeyMappings.Joy_Mapping_Right, 'Mapping_Right');
+                set('Key_Action_A', this.KeyMappings.Joy_Mapping_Action_A, 'Mapping_Action_A');
+                set('Key_Action_B', this.KeyMappings.Joy_Mapping_Action_B, 'Mapping_Action_B');
+                set('Key_Action_Start', this.KeyMappings.Joy_Mapping_Action_Start, 'Mapping_Action_Start');
+                set('Key_Action_Select', this.KeyMappings.Joy_Mapping_Action_Select, 'Mapping_Action_Select');
+                set('Key_Action_L', this.KeyMappings.Joy_Mapping_Action_L, 'Mapping_Action_L');
+                set('Key_Action_R', this.KeyMappings.Joy_Mapping_Action_R, 'Mapping_Action_R');
 
                 // Axes for D-pad analog fallback
                 try {
                     let hx = gp.axes[0], vy = gp.axes[1];
-                    if (hx < -.5) { if (!this.Key_Left)  this.sendKeyDownEvent(this.KeyMappings.Mapping_Left); }
-                    else           { if (this.Key_Left)   this.sendKeyUpEvent(this.KeyMappings.Mapping_Left); }
-                    if (hx >  .5) { if (!this.Key_Right) this.sendKeyDownEvent(this.KeyMappings.Mapping_Right); }
-                    else           { if (this.Key_Right)  this.sendKeyUpEvent(this.KeyMappings.Mapping_Right); }
-                    if (vy >  .5) { if (!this.Key_Down)  this.sendKeyDownEvent(this.KeyMappings.Mapping_Down); }
-                    else           { if (this.Key_Down)   this.sendKeyUpEvent(this.KeyMappings.Mapping_Down); }
-                    if (vy < -.5) { if (!this.Key_Up)    this.sendKeyDownEvent(this.KeyMappings.Mapping_Up); }
-                    else           { if (this.Key_Up)     this.sendKeyUpEvent(this.KeyMappings.Mapping_Up); }
-                } catch (e) {}
+                    if (hx < -.5) { if (!this.Key_Left) this.sendKeyDownEvent(this.KeyMappings.Mapping_Left); }
+                    else { if (this.Key_Left) this.sendKeyUpEvent(this.KeyMappings.Mapping_Left); }
+                    if (hx > .5) { if (!this.Key_Right) this.sendKeyDownEvent(this.KeyMappings.Mapping_Right); }
+                    else { if (this.Key_Right) this.sendKeyUpEvent(this.KeyMappings.Mapping_Right); }
+                    if (vy > .5) { if (!this.Key_Down) this.sendKeyDownEvent(this.KeyMappings.Mapping_Down); }
+                    else { if (this.Key_Down) this.sendKeyUpEvent(this.KeyMappings.Mapping_Down); }
+                    if (vy < -.5) { if (!this.Key_Up) this.sendKeyDownEvent(this.KeyMappings.Mapping_Up); }
+                    else { if (this.Key_Up) this.sendKeyUpEvent(this.KeyMappings.Mapping_Up); }
+                } catch (e) { }
             }
-        } catch (e) {}
+        } catch (e) { }
     }
 
     sendKeyDownEvent(key) {
@@ -206,10 +282,10 @@ class InputController {
 
     _normalizeArrow(event) {
         let key = event.key;
-        if (key === 'Left'  && this.KeyMappings.Mapping_Left  === 'ArrowLeft')  key = 'ArrowLeft';
+        if (key === 'Left' && this.KeyMappings.Mapping_Left === 'ArrowLeft') key = 'ArrowLeft';
         if (key === 'Right' && this.KeyMappings.Mapping_Right === 'ArrowRight') key = 'ArrowRight';
-        if (key === 'Up'    && this.KeyMappings.Mapping_Up    === 'ArrowUp')    key = 'ArrowUp';
-        if (key === 'Down'  && this.KeyMappings.Mapping_Down  === 'ArrowDown')  key = 'ArrowDown';
+        if (key === 'Up' && this.KeyMappings.Mapping_Up === 'ArrowUp') key = 'ArrowUp';
+        if (key === 'Down' && this.KeyMappings.Mapping_Down === 'ArrowDown') key = 'ArrowDown';
         return key;
     }
 
@@ -219,34 +295,34 @@ class InputController {
         ic.Key_Last = key;
         if (ic.DebugKeycodes) console.log('key:', key);
 
-        if (key === ic.KeyMappings.Mapping_Up)            ic.Key_Up = true;
-        if (key === ic.KeyMappings.Mapping_Down)          ic.Key_Down = true;
-        if (key === ic.KeyMappings.Mapping_Left)          ic.Key_Left = true;
-        if (key === ic.KeyMappings.Mapping_Right)         ic.Key_Right = true;
-        if (key === ic.KeyMappings.Mapping_Action_A)      ic.Key_Action_A = true;
-        if (key === ic.KeyMappings.Mapping_Action_B)      ic.Key_Action_B = true;
-        if (key === ic.KeyMappings.Mapping_Action_Start)  ic.Key_Action_Start = true;
+        if (key === ic.KeyMappings.Mapping_Up) ic.Key_Up = true;
+        if (key === ic.KeyMappings.Mapping_Down) ic.Key_Down = true;
+        if (key === ic.KeyMappings.Mapping_Left) ic.Key_Left = true;
+        if (key === ic.KeyMappings.Mapping_Right) ic.Key_Right = true;
+        if (key === ic.KeyMappings.Mapping_Action_A) ic.Key_Action_A = true;
+        if (key === ic.KeyMappings.Mapping_Action_B) ic.Key_Action_B = true;
+        if (key === ic.KeyMappings.Mapping_Action_Start) ic.Key_Action_Start = true;
         if (key === ic.KeyMappings.Mapping_Action_Select) ic.Key_Action_Select = true;
-        if (key === ic.KeyMappings.Mapping_Action_L)      ic.Key_Action_L = true;
-        if (key === ic.KeyMappings.Mapping_Action_R)      ic.Key_Action_R = true;
-        if (key === ic.KeyMappings.Mapping_Menu)          ic.Key_Menu = true;
+        if (key === ic.KeyMappings.Mapping_Action_L) ic.Key_Action_L = true;
+        if (key === ic.KeyMappings.Mapping_Action_R) ic.Key_Action_R = true;
+        if (key === ic.KeyMappings.Mapping_Menu) ic.Key_Menu = true;
     }
 
     keyUp(event) {
         let ic = this;
         const key = ic._normalizeArrow(event);
 
-        if (key === ic.KeyMappings.Mapping_Up)            ic.Key_Up = false;
-        if (key === ic.KeyMappings.Mapping_Down)          ic.Key_Down = false;
-        if (key === ic.KeyMappings.Mapping_Left)          ic.Key_Left = false;
-        if (key === ic.KeyMappings.Mapping_Right)         ic.Key_Right = false;
-        if (key === ic.KeyMappings.Mapping_Action_A)      ic.Key_Action_A = false;
-        if (key === ic.KeyMappings.Mapping_Action_B)      ic.Key_Action_B = false;
-        if (key === ic.KeyMappings.Mapping_Action_Start)  ic.Key_Action_Start = false;
+        if (key === ic.KeyMappings.Mapping_Up) ic.Key_Up = false;
+        if (key === ic.KeyMappings.Mapping_Down) ic.Key_Down = false;
+        if (key === ic.KeyMappings.Mapping_Left) ic.Key_Left = false;
+        if (key === ic.KeyMappings.Mapping_Right) ic.Key_Right = false;
+        if (key === ic.KeyMappings.Mapping_Action_A) ic.Key_Action_A = false;
+        if (key === ic.KeyMappings.Mapping_Action_B) ic.Key_Action_B = false;
+        if (key === ic.KeyMappings.Mapping_Action_Start) ic.Key_Action_Start = false;
         if (key === ic.KeyMappings.Mapping_Action_Select) ic.Key_Action_Select = false;
-        if (key === ic.KeyMappings.Mapping_Action_L)      ic.Key_Action_L = false;
-        if (key === ic.KeyMappings.Mapping_Action_R)      ic.Key_Action_R = false;
-        if (key === ic.KeyMappings.Mapping_Menu)          ic.Key_Menu = false;
+        if (key === ic.KeyMappings.Mapping_Action_L) ic.Key_Action_L = false;
+        if (key === ic.KeyMappings.Mapping_Action_R) ic.Key_Action_R = false;
+        if (key === ic.KeyMappings.Mapping_Menu) ic.Key_Menu = false;
     }
 
     update() {
@@ -263,16 +339,16 @@ class InputController {
     // Bit order matches what the GBA emulator expects (adapt if needed).
     updateMobileControls() {
         let s = '';
-        s += this.Key_Up            ? '1' : '0'; // UP
-        s += this.Key_Down          ? '1' : '0'; // DOWN
-        s += this.Key_Left          ? '1' : '0'; // LEFT
-        s += this.Key_Right         ? '1' : '0'; // RIGHT
-        s += this.Key_Action_A      ? '1' : '0'; // A
-        s += this.Key_Action_B      ? '1' : '0'; // B
-        s += this.Key_Action_Start  ? '1' : '0'; // START
+        s += this.Key_Up ? '1' : '0'; // UP
+        s += this.Key_Down ? '1' : '0'; // DOWN
+        s += this.Key_Left ? '1' : '0'; // LEFT
+        s += this.Key_Right ? '1' : '0'; // RIGHT
+        s += this.Key_Action_A ? '1' : '0'; // A
+        s += this.Key_Action_B ? '1' : '0'; // B
+        s += this.Key_Action_Start ? '1' : '0'; // START
         s += this.Key_Action_Select ? '1' : '0'; // SELECT
-        s += this.Key_Action_L      ? '1' : '0'; // L
-        s += this.Key_Action_R      ? '1' : '0'; // R
+        s += this.Key_Action_L ? '1' : '0'; // L
+        s += this.Key_Action_R ? '1' : '0'; // R
         window['myApp'].sendMobileControls(s, this.VectorX.toString(), this.VectorY.toString());
     }
 }
